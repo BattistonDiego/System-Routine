13c587e5e2d60e47d827b66c05bae596591bf172
Battiston100 <dbattiston78@gmail.com>
2025-10-31 16:56:50 -0300

    implementando os cards de progresso etc

--- src/app/features/habts-page/habts-page.ts
+++ src/app/features/habts-page/habts-page.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { Card } from '../../components/card/card';
 
 @Component({
   selector: 'app-habts-page',
-  imports: [MatIconModule, MatButtonModule, CommonModule],
+  imports: [MatIconModule, MatButtonModule, CommonModule, Card],
   templateUrl: './habts-page.html',
   styleUrl: './habts-page.scss',
 })
 export class HabtsPage {
   selectedDate = new Date();
+
+  listCards = [
+    { description: 'Progresso', complement: '70%', icon: 'assets/png/icon-progress.png' },
+    {
+      description: 'Hábitos Completos',
+      complement: '0/3',
+      icon: 'assets/png/icon-complete.png',
+    },
+    { description: 'Sequência', complement: '3 Dias', icon: 'assets/png/icon-sequence.png' },
+  ];
 
   changeDate(days: number) {
     const newDate = new Date(this.selectedDate);
